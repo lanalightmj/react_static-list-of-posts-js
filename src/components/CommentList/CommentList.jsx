3628860fdca6c3f7e4a70ef1bd90313648f96981
lanalightmj <lanalightmj@i.ua>
2025-03-29 09:45:10 +0200

    second commit

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -3,7 +3,7 @@ import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
-    {comments.map(comment => (
+    {comments.map((comment) => (
       <CommentInfo key={comment.id} comment={comment} />
     ))}
   </div>
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -2,7 +2,7 @@ import { PostInfo } from '../PostInfo/PostInfo';
 
 export const PostList = ({ posts }) => (
   <div className="PostList">
-    {posts.map(post => (
+    {posts.map((post) => (
       <PostInfo key={post.id} post={post} />
     ))}
   </div>
